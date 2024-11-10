9b412aa836de0b1902b94bead8fac167d96c6ceb
mehejar <abdalmehejar@gmail.com>
2024-11-10 23:33:44 +0600

    Uses of Generic type in function

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -20,11 +20,34 @@
         id: 986
     })
 
-    const createUserWithTuple = <T, Q> (param1: T , param2: Q) : [T, Q] =>{
-        return[param1, param2]
+    const createUserWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+        return [param1, param2]
     }
 
-    const res10 = createUserWithTuple<string, number> ('Bnagladesh', 3634)
+    const res10 = createUserWithTuple<string, number>('Bnagladesh', 3634)
 
+
+    const newStudentInfo = <T>(student: T) => {
+        const course: string = 'Next Level Web Development'
+
+        return {
+            ...student,
+            course
+
+        }
+    }
+
+    const student1 = newStudentInfo<{ name: string, level: string, id: number }>({ name: 'Mehejar', level: 'NExt level', id: 736473 })
+
+    console.log(student1)
     // 
+
+    const newUserInfo = <T> (userInfo: T)  => {
+        const from: string = 'USA'
+        return [userInfo, from]
+    }
+
+    const user1 = newUserInfo<string>('Masud')
+
+    console.log(user1)
 }
